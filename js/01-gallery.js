5c7637fa0322a1eb6664c0a04bddc39e4a414f93
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-07-20 22:37:53 +0300

    solution 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,10 +7,11 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 console.log(gallery)
 
+
 function createElementOfGallery(items) {
-     return galleryItems.map(({ original, preview, description }) => {
+     return items.map(({ original, preview, description }) => {
         return `<li class="gallery__item">
-     <a class="gallery__link"  href="${original}">
+     <a class="gallery__link"    href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -21,9 +22,38 @@ function createElementOfGallery(items) {
 </li>`
     }).join('');
 
-  
-
 };
 const galleryImage = createElementOfGallery(galleryItems);
 
 gallery.insertAdjacentHTML('afterbegin', galleryImage);
+
+gallery.addEventListener('click', onImageClick);
+
+ 
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  } else {
+    window.addEventListener('keydown', onEscKeypress);
+    const result = basicLightbox.create(`
+    <img src=${event.target.dataset.source}  >
+`)
+    result.show();
+   
+  };
+ window.removeEventListener('keydown', onEscKeypress);
+};
+
+ 
+
+function onEscKeypress(event) {
+  console.log(event)
+ 
+  // if (event.code === 'Escape') {
+    
+  //  console.log('Закрыть модалку')
+  // };
+// return
+};
