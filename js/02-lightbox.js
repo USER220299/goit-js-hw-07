11e011cb590ec75aa2483055ade568282ab0589a
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-07-21 22:49:52 +0300

    edit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-
-
 console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
+
 const galleryImage = createElementOfGallery(galleryItems);
+
 gallery.insertAdjacentHTML('afterbegin', galleryImage);
+
 gallery.addEventListener('click', onImageClick);
 
 function createElementOfGallery(items) {
